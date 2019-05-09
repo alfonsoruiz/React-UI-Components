6f7114d4e26b8008bf5ccab89d2e1bff922e7abe
Alfonso Ruiz <1800ruiz@gmail.com>
2019-05-08 20:31:43 -0700

    created project 1 social card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+    return (
+        <div className="card-content">
+            <h2 className="card-heading">Get Started With React</h2>
+            <p className="card-description">React makes it painless to create interactive UIs. Design simple views for each state in your application</p>
+            <a className="card-link" href="https://www.reactjs.org">reactjs.org</a>
+        </div>
+    );
+};
+
+export default CardContent;
