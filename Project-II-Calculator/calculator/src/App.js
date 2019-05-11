363eb4bc61c519fef9e446645a1cace9b093e1f7
Alfonso Ruiz <1800ruiz@gmail.com>
2019-05-10 21:35:22 -0700

    created and styled calculator UI components

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator">
+      <div className="keypad">
+        <CalculatorDisplay/>
+        <ActionButton buttonStyle={'action'} text={'clear'}/>
+        <NumberButton buttonStyle={'operator'} text={'%'}/>
+        <NumberButton buttonStyle={'integer'}  text={7}/>
+        <NumberButton buttonStyle={'integer'} text={8}/>
+        <NumberButton buttonStyle={'integer'} text={9}/>
+        <NumberButton buttonStyle={'operator'} text={'x'}/>
+        <NumberButton buttonStyle={'integer'} text={4}/>
+        <NumberButton buttonStyle={'integer'} text={5}/>
+        <NumberButton buttonStyle={'integer'} text={6}/>
+        <NumberButton buttonStyle={'operator'} text={'-'}/>
+        <NumberButton buttonStyle={'integer'} text={1}/>
+        <NumberButton buttonStyle={'integer'} text={2}/>
+        <NumberButton buttonStyle={'integer'} text={3}/>
+        <NumberButton buttonStyle={'operator'} text={'+'}/>
+        <ActionButton buttonStyle={'action'} text={0}/>
+        <NumberButton buttonStyle={'operator'} text={'='}/>
+      </div>
     </div>
   );
 };
